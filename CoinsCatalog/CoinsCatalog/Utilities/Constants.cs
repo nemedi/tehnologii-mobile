d54bd88b7a0caba578e33e9ad652735c33f18b71
Iulian Ilie-Nemedi <ilie.nemedi.iulian@gmail.com>
2023-03-04 00:49:54 +0200

    INT - Cleanup.

--- CoinsCatalog/CoinsCatalog/Utilities/Constants.cs
+++ CoinsCatalog/CoinsCatalog/Utilities/Constants.cs
@@ -8,6 +8,6 @@ namespace CoinsCatalog.Utilities
 {
     internal class Constants
     {
-        public const string ApiKey = "KWZ7ntqJygm6aWYgpSB1jAFe45dFXxwRxtFr2M57";
+        public const string ApiKey = "";
     }
 }
